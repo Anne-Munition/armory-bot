a040ee5fb736e248a35395e605e1dc9afa05ecff
DBKynd <db@dbkynd.com>
2016-10-07 22:41:09 -0600

    Rename config files

---
+++ example-config.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  // Your Discord ID, NOT the bot's ID
+  owner_id: '',
+  // The Bot's Discord access token
+  bot_token: '',
+  // The Bot's application ID
+  bot_app_id: '',
+  commands: {
+    // Prefix to be used for all commands
+    prefix: '!',
+    // Newly created commands will have deny permissions by default
+    deny_blank_perms: false,
+  },
+  // Your Twitter app's credentials
+  twitter: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: '',
+  },
+  // The mongoDB URI for database storage
+  mongodb_uri: '',
+  // This tag is hidden from searches for privacy
+  battle_net_tag: '',
+  // Nodecraft credentials for showing MC server statistics
+  nodecraft: {
+    username: '',
+    api_key: '',
+    server_id: '',
+  },
+};

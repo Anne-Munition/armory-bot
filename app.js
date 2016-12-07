a63745126ecf4208b640bc235701a5f46a59d554
DBKynd <db@dbkynd.com>
2016-12-06 23:30:48 -0700

    Put Discord notification into seperate module
    guildCreate guildDelete guildMemberAdd guildMemberRemove

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const logger = require('./lib/logger')();
 require('./lib');
 
 process.on('unhandledRejection', err => {
-  logger.error(`Uncaught Promise Error: \n${err ? err.stack : ''}`);
+  logger.error(`Uncaught Error: \n${err ? err.stack : ''}`);
 });

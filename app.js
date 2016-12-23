38900e6d42f0b204551a34963cd346f573ed5dfa
DBKynd <db@dbkynd.com>
2016-12-22 23:02:34 -0700

    Owner error type
    Get type of error to help debug.

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
 'use strict';
-const logger = require('./lib/logger')();
 require('./lib');
-
-process.on('unhandledRejection', err => {
-  logger.error(`Uncaught Error: \n${err ? err.stack : ''}`);
-});

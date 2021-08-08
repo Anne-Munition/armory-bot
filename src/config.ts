b9edb8437c641ca70e9d4fbf906fd8329c180c27
DBKynd <db@dbkynd.com>
2021-08-08 16:56:08 -0600

    update development channels

--- src/config.ts
+++ src/config.ts
@@ -6,19 +6,19 @@ export const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017'
 export const scheduleChannel: Snowflake =
   process.env.NODE_ENV === 'production'
     ? '362349719663542272'
-    : '140025699867164673'
+    : '872986945788211231'
 
 export const spoilerChannel: Snowflake =
   process.env.NODE_ENV === 'production'
     ? '148124154602717193'
-    : '870178320107601951'
+    : '872987015677898813'
 
 export const modChannel: Snowflake =
   process.env.NODE_ENV === 'production'
     ? '763506251291557918'
-    : '870481765607813151'
+    : '872987096925757440'
 
 export const muteRole: Snowflake =
   process.env.NODE_ENV === 'production'
     ? '706906565784895509'
-    : '872356458887532584'
+    : '835696708657872906'

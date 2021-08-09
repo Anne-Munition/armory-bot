e6694ef9adf1d5b3f38b3e8e8dd8431319887fa1
DBKynd <db@dbkynd.com>
2021-08-08 23:41:04 -0600

    twitch msg command
    move to slash once slash permissions handler exists

--- src/database/services/twitch_channel_service.ts
+++ src/database/services/twitch_channel_service.ts
@@ -6,15 +6,15 @@ async function list(): Promise<TwitchChannelDoc[]> {
   return TwitchChannel.find({})
 }
 
-async function get(channel: string): Promise<TwitchChannelDoc | null> {
-  const r = new RegExp(channel, 'i')
+async function get(twitchChannel: string): Promise<TwitchChannelDoc | null> {
+  const r = new RegExp(twitchChannel, 'i')
   return TwitchChannel.findOne({ display_name: { $regex: r } })
 }
 
 async function add(
   user: HelixUser,
   discordData: { guild_id: Snowflake; channel_id: Snowflake },
-  color: HexColorString | undefined,
+  color?: HexColorString,
 ): Promise<void> {
   await new TwitchChannel({
     display_name: user.display_name,
@@ -26,8 +26,8 @@ async function add(
   }).save()
 }
 
-async function remove(doc: TwitchChannelDoc): Promise<void> {
-  await doc.remove()
+async function remove(id: string): Promise<void> {
+  await TwitchChannel.findByIdAndDelete(id)
 }
 
 async function save(doc: TwitchChannelDoc): Promise<void> {
---
+++ src/msgCmds/twitch.ts
@@ -0,0 +1,248 @@
+import Discord from 'discord.js'
+import { TwitchChannelDoc } from '../database/models/twitch_channel_model'
+import TwitchChannel from '../database/services/twitch_channel_service'
+import logger from '../logger'
+import getChannelColor from '../twitch/getChannelColor'
+import { getUsers } from '../twitch/twitch_api'
+import { displayName, makePossessive, usage } from '../utilities'
+
+export const info: CmdInfo = {
+  desc: 'Manage which channels post when Twitch streams go live.',
+  usage: '<add | remove | list>',
+  aliases: [],
+  hidden: true,
+  permissions: ['SEND_MESSAGES'],
+  dmAllowed: false,
+  paramsRequired: true,
+}
+
+export const run: Run = async function (msg, params, cmd): Promise<void> {
+  params = params.map((p) => p.toLowerCase())
+
+  switch (params[0]) {
+    case 'add':
+      if (params.length < 2) {
+        cmd.info.usage = `${params[0]} <twitchChannel>`
+        return usage(msg, cmd)
+      }
+      return await addChannel(msg, params[1])
+    case 'remove':
+      if (params.length < 2) {
+        cmd.info.usage = `${params[0]} <twitchChannel>`
+        return usage(msg, cmd)
+      }
+      return await removeChannel(msg, params[1])
+    case 'list':
+      return await list(msg, params)
+    default:
+      return usage(msg, cmd)
+  }
+}
+
+async function list(msg: Discord.Message, params: string[]): Promise<void> {
+  const guild = msg.guild
+  if (!guild) return
+
+  let results
+  try {
+    results = await TwitchChannel.list()
+  } catch (err) {
+    await msg.reply('Database error. Please try again.')
+    return
+  }
+  logger.debug(`twitch list results: ${results.length}`)
+  if (!results.length) {
+    await msg.reply(
+      'No Twitch channels are currently posting when they go live.',
+    )
+    return
+  }
+
+  let str = ''
+  if (params[1] === 'all' && msg.author.id === process.env.OWNER_ID) {
+    logger.debug('list all guilds')
+    results.forEach((n) => {
+      const channels: Discord.GuildChannel[] = []
+      n.discord_channels.forEach((c) => {
+        const guild = msg.client.guilds.cache.get(c.guild_id)
+        if (!guild) return
+        const channel = guild.channels.cache.get(c.channel_id)
+        if (channel && channel.type === 'GUILD_TEXT') channels.push(channel)
+      })
+
+      channels
+        .sort((a, b) => a.position - b.position)
+        .map((x) => `${x.guild.name} - **#${x.name}**`)
+      if (channels.length > 0) {
+        str += `${getListHeader(n.display_name)}${channels.join('\n')}\n\n`
+      }
+    })
+  } else {
+    logger.debug('list this guild')
+    const thisGuild = results.filter((n) => {
+      const s = n.discord_channels.filter((c) => c.guild_id === guild.id)
+      return !(s === null)
+    })
+    thisGuild.forEach((n) => {
+      const channels: Discord.GuildChannel[] = []
+      n.discord_channels
+        .filter((c) => c.guild_id === guild.id)
+        .forEach((c) => {
+          const channel = guild.channels.cache.get(c.channel_id)
+          if (channel && channel.type === 'GUILD_TEXT') channels.push(channel)
+        })
+      channels.sort((a, b) => a.position - b.position).map((x) => x.toString())
+      if (channels.length > 0) {
+        str += `${getListHeader(n.display_name)}${channels.join('\n')}\n\n`
+      }
+    })
+  }
+  if (!str) {
+    await msg.channel.send(
+      'No Twitch channels are currently posting when they go live.',
+    )
+    return
+  }
+  const split = Discord.Util.splitMessage(str, { maxLength: 1800 })
+  for (let i = 0; i < split.length; i++) {
+    await msg.channel.send(split[i])
+  }
+}
+
+async function getDocument(
+  msg: Discord.Message,
+  twitchChannel: string,
+): Promise<TwitchChannelDoc | null | undefined> {
+  try {
+    return TwitchChannel.get(twitchChannel)
+  } catch (err) {
+    logger.error(err)
+    await msg.reply('Database error. Please try again.')
+  }
+}
+
+async function addChannel(
+  msg: Discord.Message,
+  channel: string,
+): Promise<void> {
+  const guild = msg.guild
+  if (!guild) return
+  const result = await getDocument(msg, channel)
+  if (result) {
+    const chan = result.discord_channels.filter(
+      (x) => x.guild_id === guild.id && x.channel_id === msg.channel.id,
+    )
+    if (!chan.length) {
+      result.discord_channels.push({
+        guild_id: guild.id,
+        channel_id: msg.channel.id,
+      })
+      try {
+        await TwitchChannel.save(result)
+      } catch (err) {
+        logger.error(err)
+        msg.channel.send('There was a database error, please try again.')
+        return
+      }
+      msg.channel.send(
+        `This channel will now be notified when **${result.display_name}** ` +
+          `goes live on Twitch.`,
+      )
+    } else {
+      await msg.channel.send(
+        `This channel already gets notified when ` +
+          `**${result.display_name}** goes live on Twitch.`,
+      )
+    }
+  } else {
+    const [user] = await getUsers([channel])
+    if (!user) {
+      msg.channel.send(`**${channel}** is not a known Twitch channel.`)
+      return
+    }
+    let color
+    try {
+      color = await getChannelColor(user)
+    } catch (err) {
+      // Do Nothing
+    }
+    try {
+      await TwitchChannel.add(
+        user,
+        {
+          guild_id: guild.id,
+          channel_id: msg.channel.id,
+        },
+        color,
+      )
+    } catch (err) {
+      logger.error(err)
+      msg.channel.send('There was a database error, please try again.')
+      return
+    }
+    msg.channel.send(
+      `This channel will now be notified when **${user.display_name}** goes live on Twitch.\n\nWe have not yet synced with this Twitch channel.\nAn initial message may post within a few minutes if the channel is currently live.`,
+    )
+  }
+}
+
+async function removeChannel(
+  msg: Discord.Message,
+  twitchChannel: string,
+): Promise<void> {
+  const result = await getDocument(msg, twitchChannel)
+  if (result) {
+    let index = -1
+    for (let i = 0; i < result.discord_channels.length; i++) {
+      if (result.discord_channels[i].channel_id === msg.channel.id) {
+        index = i
+      }
+    }
+    if (index === -1) {
+      await msg.channel.send(
+        `This channel is not notified when **${result.display_name}** ` +
+          `goes live on Twitch.`,
+      )
+      return
+    }
+    result.discord_channels.splice(index, 1)
+    if (result.discord_channels.length === 0) {
+      try {
+        await TwitchChannel.remove(result.id)
+        msg.channel.send(
+          `This channel will no longer be notified when ` +
+            `**${result.display_name}** goes live on Twitch.`,
+        )
+      } catch (err) {
+        logger.error(err)
+        msg.channel.send('There was a database error, please try again.')
+      }
+    } else {
+      try {
+        await TwitchChannel.save(result)
+        msg.channel.send(
+          `This channel will no longer be notified when ` +
+            `**${result.display_name}** goes live on Twitch.`,
+        )
+      } catch (err) {
+        logger.error(err)
+        msg.channel.send('There was a database error, please try again.')
+      }
+    }
+  } else {
+    const [user] = await getUsers([twitchChannel])
+    if (!user) {
+      msg.channel.send(`**${twitchChannel}** is not a known Twitch channel.`)
+      return
+    }
+    const name = displayName(user)
+    msg.channel.send(
+      `No channels are set to be notified when **${name}** ` +
+        `goes live on Twitch.`,
+    )
+  }
+}
+
+function getListHeader(name: string): string {
+  return `**${makePossessive(name)}** Twitch streams post to:\n`
+}

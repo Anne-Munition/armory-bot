488fca76a778368cd510797eb5bee937c01729f1
DBKynd <db@dbkynd.com>
2021-08-25 16:06:49 -0600

    re-enable message handler

--- src/messages/message_handler.ts
+++ src/messages/message_handler.ts
@@ -7,16 +7,14 @@ import spoilerTags from './spoiler_tags'
 export default async function (msg: Message): Promise<void> {
   counts.increment('messagesSeen')
 
-  return // TODO
-
   const wasSchedule = await schedulePinning(msg).catch((err) => {
-    log.error(err)
+    log.error(err.stack || err.message || err)
   })
   if (wasSchedule) return
 
   if (msg.author.bot) return
 
   spoilerTags(msg).catch((err) => {
-    log.error(err.stack)
+    log.error(err.stack || err.message || err)
   })
 }

b056addaa9aa11d76ab980bc1db73ab3e9d63e35
DBKynd <db@dbkynd.com>
2021-08-24 18:18:42 -0600

    make all whatnow messages ephemeral

--- src/commands/whatnow.ts
+++ src/commands/whatnow.ts
@@ -18,24 +18,21 @@ export const structure: CmdStructure = {
 }
 
 export const run: CmdRun = async (interaction): Promise<void> => {
+  await interaction.deferReply({ ephemeral: true })
+
   const liveSubs: LiveSubs = await axios
     .get('https://info.annemunition.tv/getLiveSubs')
     .then(({ data }) => data)
 
   if (!liveSubs) {
-    await interaction.reply({
-      content: 'Unable to get a valid response.',
-      ephemeral: true,
-    })
+    await interaction.editReply('Unable to get a valid response.')
     return
   }
 
   if (!liveSubs.length) {
-    await interaction.reply('No subs are currently live.')
+    await interaction.editReply('No subs are currently live.')
     return
   }
-
-  await interaction.deferReply({ ephemeral: true })
 
   const strings = liveSubs
     // Sort by display_name
@@ -63,7 +60,7 @@ export const run: CmdRun = async (interaction): Promise<void> => {
     if (i === 0) {
       await interaction.editReply(split[i])
     } else {
-      await interaction.followUp(split[i])
+      await interaction.followUp({ content: split[i], ephemeral: true })
     }
   }
 }

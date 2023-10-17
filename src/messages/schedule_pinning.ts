5f849623eb2233bcdc079181e822069f36cdde20
DBKynd <db@dbkynd.com>
2023-10-17 01:29:23 -0600

    schedule pinning with twitter embeds

--- src/messages/schedule_pinning.ts
+++ src/messages/schedule_pinning.ts
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import { Message } from 'discord.js';
 import { getId } from '../config';
 
+const reg = /https?:\/\/(([vf]x)?twitter).com/;
 export default async function (msg: Message): Promise<boolean> {
   if (msg.guildId && msg.channel.id === getId(msg.guildId, 'scheduleChannel')) {
-    if (contentHasScheduleTerms(msg)) {
+    if (contentHasScheduleTerms(msg.content)) {
       const pinnedMessages = await msg.channel.messages.fetchPinned();
       if (pinnedMessages.size)
         pinnedMessages.forEach((x) => {
@@ -11,11 +13,33 @@ export default async function (msg: Message): Promise<boolean> {
         });
       await msg.pin();
       return true;
+    } else if (reg.test(msg.content)) {
+      const matches = msg.content.match(reg) || [];
+      if (!matches[1]) return false;
+      const url = msg.content.replace(matches[1], 'api.vxtwitter');
+      const index = url.indexOf('?');
+      const trimmedUrl = url.substring(0, index);
+      axios
+        .get(trimmedUrl)
+        .then(async ({ data }) => {
+          if (contentHasScheduleTerms(data.text)) {
+            const pinnedMessages = await msg.channel.messages.fetchPinned();
+            if (pinnedMessages.size)
+              pinnedMessages.forEach((x) => {
+                x.unpin();
+              });
+            await msg.pin();
+            return true;
+          }
+        })
+        .then(() => {
+          return false;
+        });
     }
   }
   return false;
 }
 
-function contentHasScheduleTerms(msg: Message): boolean {
-  return msg.content.includes('SCHEDULE');
+function contentHasScheduleTerms(text: string): boolean {
+  return text.includes('SCHEDULE');
 }

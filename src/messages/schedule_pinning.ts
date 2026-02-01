9efcc8c5bd30edeb5cff960f95b056d3e07f9095
DBKynd <db@dbkynd.com>
2026-02-01 13:07:12 -0700

    update schedule pinning for reddit

--- src/messages/schedule_pinning.ts
+++ src/messages/schedule_pinning.ts
@@ -1,33 +1,21 @@
-import axios from 'axios';
 import { Message } from 'discord.js';
 import { getId } from '../config.js';
 
-const reg = /https?:\/\/(([vf]x)?twitter).com/;
 export default async function (msg: Message): Promise<boolean> {
-  if (msg.guildId && msg.channel.id === getId(msg.guildId, 'scheduleChannel')) {
-    const pinnedMessages = await msg.channel.messages.fetchPinned();
+  if (
+    msg.guildId &&
+    msg.channel.id === getId(msg.guildId, 'scheduleChannel') &&
+    msg.author.id === '1343656685943918642' // r/AnneMunition bot ID
+  ) {
     if (contentHasScheduleTerms(msg.content)) {
-      if (pinnedMessages.size) pinnedMessages.forEach((x) => x.unpin());
+      const pinnedMessages = await msg.channel.messages.fetchPinned();
+      if (pinnedMessages.size) {
+        pinnedMessages.forEach((x) => {
+          if (contentHasScheduleTerms(x.content)) x.unpin();
+        });
+      }
       await msg.pin();
       return true;
-    } else if (reg.test(msg.content)) {
-      const matches = msg.content.match(reg) || [];
-      if (!matches[1]) return false;
-      const url = msg.content.replace(matches[1], 'api.vxtwitter');
-      const index = url.indexOf('?');
-      const trimmedUrl = url.substring(0, index);
-      axios
-        .get(trimmedUrl)
-        .then(async ({ data }) => {
-          if (contentHasScheduleTerms(data.text)) {
-            if (pinnedMessages.size) pinnedMessages.forEach((x) => x.unpin());
-            await msg.pin();
-            return true;
-          }
-        })
-        .then(() => {
-          return false;
-        });
     }
   }
   return false;
